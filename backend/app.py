2a06e657b5a9e154258c6feaec33ec80d879e099
Swapnil Lahiri <swapnil240805@gmail.com>
2025-09-14 00:30:52 +0530

    Add libraries, made postgres conns

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,39 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
+
+from flask_sqlalchemy import SQLAlchemy
+import psycopg2
+from dotenv import load_dotenv
+import os
+
+import cv2
+import face_recognition
+import numpy as np
+from PIL import Image
+
+import bcrypt
+
+from sqlalchemy import create_engine
+
 app = Flask(__name__)
-@app.route('/')
-def home():
-    return jsonify({"msg": "Nightlamps Initiated"})
-if __name__ == '__main__':
+CORS(app)
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DB_URL")
+
+
+try:
+    engine = create_engine(DATABASE_URL)
+    connection = engine.connect()
+    print("✅ Connection successful!")
+    connection.close()
+except Exception as e:
+    print("❌ Connection failed:", e)
+
+@app.route("/ping")
+def ping():
+    return jsonify({"msg": "Backend is alive!"})
+
+if __name__ == "__main__":
     app.run(debug=True)
